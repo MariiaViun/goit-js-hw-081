046f4b6010ff4ee061c34a6115bf258423e9b2c9
MariiaViun <lukrainka03@gmail.com>
2023-04-26 17:17:33 +0300

    fins

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+import '../css/03-feedback.css';
+
+const STORAGE_KEY = 'feedback-form-state';
+let formValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+  input: document.querySelector('.feedback-form input')
+};
+
+const { email, message } = refs.form.elements;
+const onFormInput = throttle(event => {
+  formValues = {
+    email: email.value,
+    message: message.value
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+}, 500);
+
+if (formValues) {
+  email.value = formValues.email;
+  message.value = formValues.message;
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormInput);
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+if (!email.value || !message.value) {
+  alert("Всі поля повинні бути заповнені");
+}
+
+console.log('Form Data:', formValues);
+
+evt.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+  
+}
+
+
 
